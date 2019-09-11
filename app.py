a1fecb1ef7cc8ad9365aa8d23635ce1c50c36431
angelinaolmedo7 <angelinaolmedo7@gmail.com>
2019-09-11 16:53:08 -0700

    Still doesnt work

--- app.py
+++ app.py
@@ -10,25 +10,18 @@ def index():
     """Return homepage."""
     # Extract query term from url
     search_term = request.args.get('search_term')
-    # TODO: Make 'params' dict with query term and API key
-    params = {'query term': search_term, 'API key': 'FFZUQ4CLKXOZ', 'limit': 10}
-    # TODO: Make an API call to Tenor using the 'requests' library
 
-    # get the top 8 GIFs for the search term
+    # Make 'params' dict with query term and API key
+    params = {'query_term': search_term, 'api_key': 'FFZUQ4CLKXOZ'}
+
+    # Make an API call to Tenor using the 'requests' library
+    # Get the first 10 results from the search results
     r = requests.get(
-    "https://api.tenor.com/v1/search?q=%s&key=%s&limit=%s" % (params['query term'], params['API key'], params['limit']))
-    if r.status_code == 200:
-        # load the GIFs using the urls for the smaller GIF sizes
-        top_10gifs = json.loads(r.content)
-        print(top_10gifs)
-    else:
-        top_10gifs = None
-
-    # TODO: Get the first 10 results from the search results
-
+        "https://api.tenor.com/v1/search?q=%s&key=%s&limit=10" % (params['query_term'], params['api_key']))
+    gifs = json.loads(r.content)['results']
     # TODO: Render the 'index.html' template, passing the gifs as a named parameter
 
-    return render_template("index.html") + str(search_term)
+    return render_template("index.html", gifs=gifs)
 
 
 if __name__ == '__main__':
