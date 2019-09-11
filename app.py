f4b775c7e09c5b596de7f5e2a2b5b63305f7703f
anikamorris <anikamorris11@gmail.com>
2019-09-11 15:52:26 -0700

    search bar working

--- app.py
+++ app.py
@@ -11,9 +11,19 @@ def index():
     # Extract query term from url
     search_term = request.args.get('search_term')
     # TODO: Make 'params' dict with query term and API key
-    params = {'query term': search_term, 'API key': 'FFZUQ4CLKXOZ'}
+    params = {'query term': search_term, 'API key': 'FFZUQ4CLKXOZ', 'limit': 10}
     # TODO: Make an API call to Tenor using the 'requests' library
-    
+
+    # get the top 8 GIFs for the search term
+    r = requests.get(
+    "https://api.tenor.com/v1/search?q=%s&key=%s&limit=%s" % (params['query term'], params['API key'], params['limit']))
+    if r.status_code == 200:
+        # load the GIFs using the urls for the smaller GIF sizes
+        top_10gifs = json.loads(r.content)
+        print(top_10gifs)
+    else:
+        top_10gifs = None
+
     # TODO: Get the first 10 results from the search results
 
     # TODO: Render the 'index.html' template, passing the gifs as a named parameter
