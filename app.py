92daf64113f76f9b2d50b931f7edaf7586794ff4
angelinaolmedo7 <angelinaolmedo7@gmail.com>
2019-09-12 11:03:49 -0700

    Updated with new starter code
    New starter code has been integrated

--- app.py
+++ app.py
@@ -12,14 +12,15 @@ def index():
     search_term = request.args.get('search_term')
 
     # Make 'params' dict with query term and API key
-    params = {'query_term': search_term, 'api_key': 'FFZUQ4CLKXOZ'}
+    params = {'q': search_term, 'key': 'FFZUQ4CLKXOZ', 'limit': 10}
 
     # Make an API call to Tenor using the 'requests' library
     # Get the first 10 results from the search results
     r = requests.get(
-        "https://api.tenor.com/v1/search?q=%s&key=%s&limit=10" % (params['query_term'], params['api_key']))
+        "https://api.tenor.com/v1/search?q=%s&key=%s&limit=%s" %
+        (params['q'], params['key'], params['limit']))
     gifs = json.loads(r.content)['results']
-    # TODO: Render the 'index.html' template, passing the gifs as a named parameter
+    # Render the 'index.html' template, passing the gifs as a named parameter
 
     return render_template("index.html", gifs=gifs)
 
