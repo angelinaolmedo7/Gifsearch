043e1089ddcc33e8bdbffc2bb7cb71241ad234aa
angelinaolmedo7 <angelinaolmedo7@gmail.com>
2019-09-17 13:29:50 -0700

    Trending Gifs
    Trending Gifs display when there is no search term

--- app.py
+++ app.py
@@ -10,6 +10,8 @@ def index():
     """Return homepage."""
     # Extract query term from url
     search_term = request.args.get('search_term')
+    print(search_term)
+    trending = search_term is None
 
     # Make 'params' dict with query term and API key
     params = {'q': search_term, 'key': 'FFZUQ4CLKXOZ', 'limit': 10}
@@ -17,17 +19,24 @@ def index():
     urls = []
     # Make an API call to Tenor using the 'requests' library
     # Get the first 10 results from the search results
+    if trending:
+        params['q'] = requests.get(
+                    "https://api.tenor.com/v1/trending_terms?key=%s&limit=%s"
+                    % (params['key'], 1))
+        # print(r.json()['results'])
     r = requests.get(
         "https://api.tenor.com/v1/search?q=%s&key=%s&limit=%s" %
         (params['q'], params['key'], params['limit']))
+
     json_data = r.json()['results']
     for i in range(len(json_data)):
+        print(i)
         url = json_data[i]['media'][0]['gif']['url']
         urls.append(url)
-    print(urls)
+    # print(urls)
     # Render the 'index.html' template, passing the gifs as a named parameter
 
-    return render_template("index.html", urls=urls)
+    return render_template("index.html", urls=urls, trending=trending)
 
 
 if __name__ == '__main__':
